26a20b8c026c4c97b5a506980d9672e303f3442a
1m1 <imi@1m1.io>
2026-04-24 09:20:49 -0600

    update

--- src/LoopOSAgentgodCommunication.jl
+++ src/LoopOSAgentgodCommunication.jl
@@ -5,9 +5,8 @@ using TheoryOfGod: ○
 using TheoryOfGodgod: god, ∃!
 using TheoryOfGodCommunication
 
-const CONTEXT = ZMQ.context()
-const CREATESOCKET = Socket(CONTEXT, REQ)
-const OBSERVESOCKET = Socket(CONTEXT, REQ)
+const CREATESOCKET = Socket(REQ)
+const OBSERVESOCKET = Socket(REQ)
 
 function init(createipc, observeipc)
     connect(CREATESOCKET, createipc)
@@ -15,27 +14,27 @@ function init(createipc, observeipc)
 end
 function create(g::god, ϕ)
     x = SA[○, ○]
-    a = TOGCommunication.∃2d(g, x, x, ϕ)
-    TOGCommunication.send(CREATESOCKET, a)
+    a = TheoryOfGodCommunication.∃2d(g, x, x, ϕ)
+    TheoryOfGodCommunication.send(CREATESOCKET, a)
     recv(CREATESOCKET)
 end
 function create2d(g::god, μ, ρ, ϕ)
-    a = TOGCommunication.∃2d(g, μ, ρ, ϕ)
-    TOGCommunication.send(CREATESOCKET, a)
+    a = TheoryOfGodCommunication.∃2d(g, μ, ρ, ϕ)
+    TheoryOfGodCommunication.send(CREATESOCKET, a)
     recv(CREATESOCKET)
 end
 function create3d(g::god, μ, ρ, ϕ)
-    a = TOGCommunication.∃3d(g, μ, ρ, ϕ)
-    TOGCommunication.send(CREATESOCKET, a)
+    a = TheoryOfGodCommunication.∃3d(g, μ, ρ, ϕ)
+    TheoryOfGodCommunication.send(CREATESOCKET, a)
     recv(CREATESOCKET)
 end
 function createNd(d, μ, ρ, ∂, ϕ)
-    a = TOGCommunication.∃Nd(d, μ, ρ, ∂, ϕ)
-    TOGCommunication.send(CREATESOCKET, a)
+    a = TheoryOfGodCommunication.∃Nd(d, μ, ρ, ∂, ϕ)
+    TheoryOfGodCommunication.send(CREATESOCKET, a)
     recv(CREATESOCKET)
 end
 function observe(g::god)
-    TOGCommunication.send(OBSERVESOCKET, g)
+    TheoryOfGodCommunication.send(OBSERVESOCKET, g)
     recv(OBSERVESOCKET)
 end
 
